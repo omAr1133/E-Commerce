1747522dfd5764a9517a832a9cc6d8f92bd54097
omara <omara@OMARAHMED>
2025-04-12 02:45:59 +0200

    Product Configurations & Db Context

---
+++ Infrastructure/Persistence/Data/Configurations/ProductConfigurations.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+namespace Persistence.Data.Configurations
+{
+    internal class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(p => p.ProductBrand)
+                 .WithMany()
+                 .HasForeignKey(p => p.BrandId);
+
+            builder.HasOne(p => p.ProductType)
+                 .WithMany()
+                 .HasForeignKey(p => p.TypeId);
+
+            builder.Property(p => p.Price)
+                .HasColumnType("decimal(10,3)");
+        }
+    }
+}
---
+++ Infrastructure/Persistence/Data/StoreDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Data
+{
+    public class StoreDbContext(DbContextOptions<StoreDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+         => modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyReference).Assembly);
+    }
+}
