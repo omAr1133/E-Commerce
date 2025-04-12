e6e348f6095211ebd06a20ed00143ec2525b1ee9
omara <omara@OMARAHMED>
2025-04-12 12:50:11 +0200

    Generic Repository

---
+++ Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    internal class GenericRepository<TEntity, TKey> (StoreDbContext context)
+        :IGenericRepository<TEntity, TKey>
+        where TEntity:BaseEntity<TKey>
+
+    {
+        public void Add(TEntity entity) => context.Set<TEntity>().Add(entity);
+        public void Update(TEntity entity) => context.Set<TEntity>().Update(entity);
+        public void Delete(TEntity entity) => context.Set<TEntity>().Remove(entity);
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync() => await context.Set<TEntity>().ToListAsync();
+
+
+        public async Task<TEntity?> GetAsync(TKey key) => await context.Set<TEntity>().FindAsync(key);
+
+
+    }
+}
