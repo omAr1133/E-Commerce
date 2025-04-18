67b6907fea48fb62a5c4f82fd0def3f535a90326
omara <omara@OMARAHMED>
2025-04-19 00:56:19 +0200

    Specifications

---
+++ Core/Services/Specification/BaseSpecifications.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Specification
+{
+    internal abstract class BaseSpecifications<T> : ISpecifications<T> where T : class
+    {
+        protected BaseSpecifications(Expression<Func<T,bool>>? criteria)
+        {
+            Criteria= criteria;
+        }
+        public Expression<Func<T, bool>> Criteria { get;  }
+
+        public List<Expression<Func<T, object>>> IncludeExpressions { get; } = [];
+
+        protected void AddInclude(Expression<Func<T, object>> include)
+        {
+            IncludeExpressions.Add(include);
+        }
+
+    }
+
+
+}
---
+++ Core/Services/Specification/ProductWithBrandAndTypeSpecifications.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Specification
+{
+    internal class ProductWithBrandAndTypeSpecifications : BaseSpecifications<Product>
+    {
+        public ProductWithBrandAndTypeSpecifications(int id)
+            : base(product=>product.Id==id)
+        {
+            AddInclude(p => p.ProductBrand);
+            AddInclude(p => p.ProductType);
+        }
+
+        public ProductWithBrandAndTypeSpecifications():base(null)
+        {
+            AddInclude(p=>p.ProductBrand);
+            AddInclude(p=>p.ProductType);
+        }
+    }
+}
